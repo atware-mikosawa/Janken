72195cb694b42b995be5b28646a21a234d8fe7a4
y-mikosawa <y-mikosawa@atware.co.jp>
2023-07-13 12:34:52 +0900

    セカンドコミット

--- src/section4/Janken.java
+++ src/section4/Janken.java
@@ -4,6 +4,7 @@ class Janken {
 	public static void main(String... args) {
 		String name1 = args[0];
 		String name2 = args[1];
+//		変更
 		System.out.println("ジャンケンぽん！");
 		System.out.println(name1 + "の手：");
 		System.out.println(name2 + "の手：");
