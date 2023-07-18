b28e98c6592d06f6c1c368572c6cd05db0ea77e2
y-mikosawa <y-mikosawa@atware.co.jp>
2023-07-18 15:57:22 +0900

    TDD開発完了

--- src/section4/Janken.java
+++ src/section4/Janken.java
@@ -5,16 +5,23 @@ class Janken {
 	private static final String C = "チョキ";
 	private static final String P = "パー";
 	String handStatus;
+	private String name1;
+	private String name2;
 
 	public static void main(String... args) {
-		String name1 = args[0];
-		String name2 = args[1];
+		Janken janken = new Janken();
+		janken.name1 = args[0];
+		janken.name2 = args[1];
 
 		System.out.println("ジャンケンぽん！");
-		System.out.println(name1 + "の手：" + G);
-		System.out.println(name2 + "の手：" + G);
-		System.out.println("結果は？");
-		System.out.println("あいこ!勝負つかず!");
+		janken.makeHandStatus();
+		String hand1 = janken.getHandStatus();
+		janken.makeHandStatus();
+		String hand2 = janken.getHandStatus();
+
+		System.out.println(janken.name1 + "の手：" + hand1);
+		System.out.println(janken.name2 + "の手：" + hand2);
+		janken.result(hand1, hand2);
 	}
 
 	//ランダムな1~3の数値を出す
@@ -27,6 +34,26 @@ class Janken {
 			this.handStatus = C;
 		} else if (rondomInt == 2) {
 			this.handStatus = P;
+		}
+	}
+
+	//勝敗の結果を返すメソッド
+	public void result(String hand1, String hand2) {
+		System.out.println("結果は？");
+		if (//name1が勝つ時
+		(hand1.equals("グー")) && (hand2.equals("チョキ"))
+				|| (hand1.equals("チョキ")) && (hand2.equals("パー"))
+				|| (hand1.equals("パー")) && (hand2.equals("グー"))) {
+			System.out.println(name1 + "さんの勝利！");
+		} else if (//name2が勝つ時
+		(hand2.equals("グー")) && (hand1.equals("チョキ"))
+				|| (hand2.equals("チョキ")) && (hand1.equals("パー"))
+				|| (hand2.equals("パー")) && (hand1.equals("グー"))) {
+			System.out.println(name2 + "さんの勝！");
+		} else if (hand1.equals(hand2)) {//あいこの時
+			System.out.println("あいこ!勝負つかず!");
+		} else {
+			System.out.println("エラーが発生しました");
 		}
 	}
 
