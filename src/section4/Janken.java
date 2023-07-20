845fa02cbf900ece1b96a6b8345fffe0bf1456d7
Taisuke Tashiro <t-tashiro@atware.co.jp>
2023-07-20 12:06:38 +0900

    Refactor Janken

--- src/section4/Janken.java
+++ src/section4/Janken.java
@@ -1,77 +1,158 @@
 package section4;
 
 class Janken {
-	private static final String ROCK = "グー";
-	private static final String SCISSORS = "チョキ";
-	private static final String PAPER = "パー";
-	String handStatus;
-	private String name1;
-	private String name2;
 
-	public static void main(String... args) {
-		Janken janken = new Janken();
-		janken.name1 = args[0];
-		janken.name2 = args[1];
+    private static final String ROCK = "グー";
+    private static final String SCISSORS = "チョキ";
+    private static final String PAPER = "パー";
 
-		System.out.println("ジャンケンぽん！");
-		janken.makeHandStatus();
-		String hand1 = janken.getHandStatus();
-		janken.makeHandStatus();
-		String hand2 = janken.getHandStatus();
+    enum Hand {
+        ROCK("グー") {
+            @Override
+            Result judgeAgainst(Hand other) {
+                return switch (other) {
+                    case ROCK -> Result.DRAW;
+                    case SCISSORS -> Result.WIN;
+                    case PAPER -> Result.LOSE;
+                };
+            }
+        },
+        SCISSORS("チョキ") {
+            @Override
+            Result judgeAgainst(Hand other) {
+                return switch (other) {
+                    case ROCK -> Result.LOSE;
+                    case SCISSORS -> Result.DRAW;
+                    case PAPER -> Result.WIN;
+                };
+            }
+        },
+        PAPER("パー") {
+            @Override
+            Result judgeAgainst(Hand other) {
+                return switch (other) {
+                    case ROCK -> Result.WIN;
+                    case SCISSORS -> Result.LOSE;
+                    case PAPER -> Result.DRAW;
+                };
+            }
+        };
 
-		System.out.println(janken.name1 + "の手：" + hand1);
-		System.out.println(janken.name2 + "の手：" + hand2);
-		janken.result(hand1, hand2);
-	}
+        private final String japaneseName;
 
-	//ランダムな1~3の数値を出す
-	public void makeHandStatus() {
-		double round = Math.random() * 2;
-		int randomInt = (int) Math.round(round);
-		if (randomInt == 0) {
-			this.handStatus = ROCK;
-		} else if (randomInt == 1) {
-			this.handStatus = SCISSORS;
-		} else if (randomInt == 2) {
-			this.handStatus = PAPER;
-		}
-	}
+        Hand(String japaneseName) {
+            this.japaneseName = japaneseName;
+        }
 
-	//勝敗の結果を返すメソッド
-	public void result(String hand1, String hand2) {
-		System.out.println("結果は？");
-		if (//name1が勝つ時
-		(hand1.equals(ROCK)) && (hand2.equals(SCISSORS))
-				|| (hand1.equals(SCISSORS)) && (hand2.equals(PAPER))
-				|| (hand1.equals(PAPER)) && (hand2.equals(ROCK))) {
-			System.out.println(name1 + "さんの勝利！");
-		} else if (//name2が勝つ時
-		(hand2.equals(ROCK)) && (hand1.equals(SCISSORS))
-				|| (hand2.equals(SCISSORS)) && (hand1.equals(PAPER))
-				|| (hand2.equals(PAPER)) && (hand1.equals(ROCK))) {
-			System.out.println(name2 + "さんの勝利！");
-		} else if (hand1.equals(hand2)) {//あいこの時
-			System.out.println("あいこ!勝負つかず!");
-		} else {
-			System.out.println("エラーが発生しました");
-		}
-	}
+        abstract Result judgeAgainst(Hand other);
 
-	//ゲッター
-	public String getHandStatus() {
-		return handStatus;
-	}
+        public String getJapaneseName() {
+            return japaneseName;
+        }
 
-	public String getG() {
-		return ROCK;
-	}
+        public static Hand decide() {
+            double round = Math.random() * 2;
+            int randomInt = (int) Math.round(round);
+            if (randomInt == 0) {
+                return ROCK;
+            } else if (randomInt == 1) {
+                return SCISSORS;
+            } else if (randomInt == 2) {
+                return PAPER;
+            }
+            throw new IllegalStateException("unexpected random value: " + randomInt);
+        }
+    }
 
-	public String getC() {
-		return SCISSORS;
-	}
+    enum Result {
+        WIN,
+        LOSE,
+        DRAW
+    }
 
-	public String getP() {
-		return PAPER;
-	}
+    private static class Player {
+        private final String name;
+        private Hand hand = null;
+
+        Player(String name) {
+            this.name = name;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void decideHand() {
+            hand = Hand.decide();
+        }
+
+        public Hand getHand() {
+            return hand;
+        }
+    }
+
+    String handStatus;
+
+    public static void main(String... args) {
+
+        var player1 = new Player(args[0]);
+        var player2 = new Player(args[1]);
+
+        System.out.println("ジャンケンぽん！");
+
+        player1.decideHand();
+        player2.decideHand();
+
+        displayHand(player1);
+        displayHand(player2);
+
+        var result = player1.getHand().judgeAgainst(player2.getHand());
+
+        displayResult(result, player1.getName(), player2.getName());
+    }
+
+    private static void displayResult(Result result, String player1Name, String player2Name) {
+        System.out.println("結果は？");
+        if (result == Result.DRAW) {
+            System.out.println("あいこ!勝負つかず!");
+            return;
+        }
+        var nameOfTheWinner = result == Result.WIN ? player1Name : player2Name;
+        System.out.printf("%sさんの勝利！%n", nameOfTheWinner);
+    }
+
+    private static void displayHand(Player player) {
+        System.out.printf("%sの手：%s%n", player.getName(), player.getHand().getJapaneseName());
+    }
+
+    //ランダムな1~3の数値を出す
+    public void makeHandStatus() {
+        double round = Math.random() * 2;
+        int randomInt = (int) Math.round(round);
+        if (randomInt == 0) {
+            this.handStatus = ROCK;
+        } else if (randomInt == 1) {
+            this.handStatus = SCISSORS;
+        } else if (randomInt == 2) {
+            this.handStatus = PAPER;
+        }
+    }
+
+    //ゲッター
+    public String getHandStatus() {
+        return handStatus;
+    }
+
+    public String getG() {
+        return ROCK;
+    }
+
+    public String getC() {
+        return SCISSORS;
+    }
+
+    public String getP() {
+        return PAPER;
+    }
 
 }
