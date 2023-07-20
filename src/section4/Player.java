23420ef5d080bdc15110b06ff6eece1a46120dda
Taisuke Tashiro <t-tashiro@atware.co.jp>
2023-07-20 12:09:59 +0900

    Move inner classes to top-level

--- src/section4/Player.java
+++ src/section4/Player.java
@@ -2,4 +2,22 @@ package section4;
 
 class Player {
 
+    private final String name;
+    private Hand hand = null;
+
+    Player(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void decideHand() {
+        hand = Hand.decide();
+    }
+
+    public Hand getHand() {
+        return hand;
+    }
 }
