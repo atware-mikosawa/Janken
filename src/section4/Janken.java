0766fc593ad49a0277f437b1b99926c82b1036ff
y-mikosawa <y-mikosawa@atware.co.jp>
2023-07-25 12:41:49 +0900

    Refクラスの追加、リファクタリング

--- src/section4/Janken.java
+++ src/section4/Janken.java
@@ -2,40 +2,19 @@ package section4;
 
 class Janken {
 
-	public static void main(String... args) {
+    public static void main(String... args) {
+        var player1 = new Player(args[0]);
+        var player2 = new Player(args[1]);
+        var ref = new Ref(args[2]);
 
-		var player1 = new Player(args[0]);
-		var player2 = new Player(args[1]);
-		var ref = new Player(args[2]);
+        ref.displayCallOut();
+        player1.decideHand();
+        player2.decideHand();
 
-		displayCallOut(ref);
+        ref.displayHand(player1);
+        ref.displayHand(player2);
 
-		player1.decideHand();
-		player2.decideHand();
-
-		displayHand(ref, player1);
-		displayHand(ref, player2);
-
-		var result = player1.getHand().judgeAgainst(player2.getHand());
-
-		displayResult(result, player1.getName(), player2.getName(), ref.getName());
-	}
-
-	private static void displayCallOut(Player ref) {
-		System.out.println( ref.getName() + "「ジャンケンぽん！」");
-	}
-
-	private static void displayHand(Player ref, Player player) {
-		System.out.printf(ref.getName() +  "「%sの手：%s%n", player.getName(), player.getHand().getJapaneseName() + "」");
-	}
-
-	private static void displayResult(Result result, String player1Name, String player2Name, String ref) {
-		System.out.println(ref + "「結果は？」");
-		if (result == Result.DRAW) {
-			System.out.println(ref + "「あいこ!勝負つかず!」");
-			return;
-		}
-		var nameOfTheWinner = result == Result.WIN ? player1Name : player2Name;
-		System.out.printf(ref + "「%sさんの勝利！」%n", nameOfTheWinner);
-	}
+        var result = player1.getHand().judgeAgainst(player2.getHand());
+        ref.displayResult(result, player1.getName(), player2.getName(), ref.getRefName());
+    }
 }
--- src/section4/Ref.java
+++ src/section4/Ref.java
@@ -1,6 +1,33 @@
 package section4;
 
 public class Ref {
-    private final String name;
+    private final String refName;
 
+    Ref(String refName) {
+        this.refName = refName;
+    }
+
+    public String getRefName() {
+        return refName;
+    }
+
+    //元々14,18行目のメソッドはprivate、なぜ？,publicにしても良いの？、なぜstaticだったの？
+    public void displayCallOut() {
+        System.out.println(this.refName + "「ジャンケンぽん！」");
+    }
+
+    public void displayHand(Player player) {
+        System.out.printf(this.refName + "「%sの手：%s%n", player.getName(), player.getHand().getJapaneseName() + "」");
+    }
+
+
+    public void displayResult(Result result, String player1Name, String player2Name, String ref) {
+        System.out.println(ref + "「結果は？」");
+        if (result == Result.DRAW) {
+            System.out.println(ref + "「あいこ!勝負つかず!」");
+            return;
+        }
+        var nameOfTheWinner = result == Result.WIN ? player1Name : player2Name;
+        System.out.printf(ref + "「%sさんの勝利！」%n", nameOfTheWinner);
+    }
 }
